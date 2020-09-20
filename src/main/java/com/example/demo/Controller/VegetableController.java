e2b80ba06911fa3077307bd02eee6d17de0c6af2
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-09-19 20:47:02 -0400

    Controller updated

--- src/main/java/com/example/demo/Controller/VegetableController.java
+++ src/main/java/com/example/demo/Controller/VegetableController.java
@@ -3,15 +3,30 @@ package com.example.demo.Controller;
 import com.example.demo.Model.Vegetable;
 import com.example.demo.Service.VegetableService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 import java.util.Optional;
-@RequestMapping("/api")
+@RequestMapping("/api/")
 @RestController
 public class VegetableController {
   @Autowired
   VegetableService vegetableService;
+
+    @RequestMapping(value = "/", method = RequestMethod.POST,consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public RedirectView postFormAnimal( Vegetable vegetable){
+        vegetableService.createVegetable(vegetable);
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("/");
+        return redirectView;
+    }
+
+
+
+
+
     @PostMapping("/")
      public void postVegetable(@RequestBody Vegetable vegetable){
         vegetableService.createVegetable(vegetable);
@@ -25,7 +40,15 @@ public class VegetableController {
        public List<Vegetable> getAllVegetables(){
         return vegetableService.getAllVegetables();
     }
-     @PutMapping("/{id}")
+
+    @RequestMapping("/a")
+    public RedirectView localRedirect() {
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("http://www.yahoo.com");
+        return redirectView;
+    }
+
+    @PutMapping("/{id}")
         public Optional<Vegetable> updateVegetable(@RequestBody Vegetable newVegetable,Long id){
         return vegetableService.updateVegetable(newVegetable,id);
      }
